37e9c0b2e8ebaa91b30f3e37c57c19ca84668bed
Steven R. Loomis <srl@icu-project.org>
2014-06-09 08:11:51 -0700

    proper structure

---
+++ README.md
@@ -0,0 +1,21 @@
+This is a very, very, very basic test of es402
+===
+
+### What is this:
+
+This tests basic sanity in ES402 intl implementation
+
+### Why does this exist
+
+This file is part of the Node.JS ICU enablement work
+https://github.com/joyent/node/pull/7719
+and is under the same license.
+
+### How do I use it
+
+    node btest402.js
+
+### Who wrote it
+
+Steven R. Loomis < srl AT icu-project DOT org >
+
--- btest402.js
+++ btest402.js
@@ -1,3 +1,12 @@
+// Copyright (C) 2014 IBM Corporation and Others. All Rights Reserved.
+// This file is part of the Node.JS ICU enablement work
+// https://github.com/joyent/node/pull/7719
+// and is under the same license.
+//
+// This is a very, very, very basic test of es402
+//
+// Usage: node btest402.js
+
 var i = Intl;
 console.log("+ Congrats, you have the Intl object.");
 var locs = [ "en", "mt", "ja" ];
@@ -6,8 +15,12 @@ for ( var n=0; n<locs.length; n++ ) {
     var loc = locs[n];
     console.log(loc+":");
     console.log(" date:" + d.toLocaleString(loc,{month: "long",day:"numeric",weekday:"long",year:"numeric"}));
-    console.log(" brk:" + 
-                Intl.v8BreakIterator.supportedLocalesOf(loc) + " first " + 
-                new Intl.v8BreakIterator(loc).first() );
+    try {
+        console.log(" brk:" + 
+                    Intl.v8BreakIterator.supportedLocalesOf(loc) + " first " + 
+                    new Intl.v8BreakIterator(loc).first() );
+    } catch ( e) {
+        console.log("brk err: " + e);
+    }
     console.log();
 }
